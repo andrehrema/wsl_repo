1f91efb9993c9f52df3b3ab2b61f5084f19b48fc
andrehrema <andre.machoski@outlook.com>
2022-03-13 19:00:34 -0300

    Errors correction

--- ESP32/joystick/main/joystick.c
+++ ESP32/joystick/main/joystick.c
@@ -22,10 +22,10 @@
 #include "esp_netif.h"
 #include "esp_sntp.h"
 #include "lwip/sockets.h"
-#include "apps/esp_sntp.h"
 
-#include "ulp_common.h"
-#include "ulp_ulpcode.h"
+#include "esp32/ulp.h"
+#include "ulp_main.h"
+#include "cJSON.h"
 
 #define GPIO_INPUT_PIN_MASK 0xFFF000
 
@@ -50,7 +50,7 @@
 
 #define PERIOD_0 0
 //period in us which ulp processor will be waken up
-#define ULP_WAKE_UP_PERIOD 75000
+#define ULP_WAKEUP_PERIOD 75000
 
 static volatile QueueHandle_t gxButtonMailbox;
 static SemaphoreHandle_t gxSerialMutex;
@@ -69,11 +69,21 @@ extern const uint8_t bin_end[]   asm("_binary_ulp_ulpcode_bin_end");
 
 typedef enum
 {
-    UNKNOwN,
+    UNKNOWN,
     RETRY,
     CONNECTED,
     CANCELLED
 }SNTP_TRIES;
+
+typedef enum
+{
+    DISCONNECTED,
+    CONNECTED,
+    SOCKET_ESTABLISHED,
+    DATA_SENT,
+    ANSWER_RECEIVED,
+    COMMUNICATION_SUCCESSFUL
+}COMMUNICATION_STATUS;
 
 void printString(const char *msg)
 {
@@ -95,39 +105,103 @@ void printStringPlusNumber(const char *msg, int number)
     printString(buffer);
 }
 
-void IRAM_ATTR GpioISRHandler(void *arg)
+
+bool vInitSocket()
 {
-    BaseType_t high_priority_task;
-    uint32_t uc_pin = (uint32_t) arg;
+    bool retVal = false;
+    gxSocketConfig.sin_len = sizeof(struct sockaddr_in);
+    gxSocketConfig.sin_family = AF_INET;
+    gxSocketConfig.sin_port = htons(UDP_PORT);
 
-    xQueueSendFromISR(gxButtonMailbox, (void *) &uc_pin, &high_priority_task);
-
-    if(high_priority_task)
+    if(-1 == (giSocketFd = socket(AF_INET, SOCK_DGRAM, 0)))
     {
-        portYIELD_FROM_ISR();
+        printString("Socket Initialization Error");
     }
+    else if(-1 == bind(giSocketFd, (struct sockaddr *) &gxSocketConfig, gxSocketConfig.sin_len))
+    {
+        printString("Socket binding error");
+    }
+    else
+        retVal = true;
+
+    return retVal;
 }
 
-void GetData(void *pvParameters)
+/*-------------------------------------------- SNTP functions ---------------------------------------------*/
+void SNTPCallbackFunction(struct timeval *tv)
 {
-    uint32_t button_pressed = 0;
-    char serial_msg[100];
-    for(;;)
+    sntp_sync_status_t sntp_status = sntp_get_sync_status();
+
+    switch(sntp_status)
     {
-        if(pdTRUE == xQueueReceive(gxButtonMailbox, (void *) &button_pressed, portMAX_DELAY))
+        case SNTP_SYNC_STATUS_RESET:
         {
-            snprintf(serial_msg, sizeof(serial_msg), "button_pressed: %i", button_pressed);
-            printString(serial_msg);
-            snprintf(serial_msg, sizeof(serial_msg), "contador: %i", giInterruptCounter);
-            printString(serial_msg);
-            button_pressed = 0;
+            xEventGroupSetBits(gxNetworkEventGroup, SNTP_RESET_STATUS);
+            break;
         }
 
-        snprintf(serial_msg, sizeof(serial_msg), "Executado SendDataWifi");
-        printString(serial_msg);
-        vTaskDelay(100/portTICK_PERIOD_MS);
+        case SNTP_SYNC_STATUS_COMPLETED:
+        {
+            xEventGroupSetBits(gxNetworkEventGroup, SNTP_CONNECTED_STATUS);
+            break;
+        }
+
+        case SNTP_SYNC_STATUS_IN_PROGRESS:
+        {
+            xEventGroupSetBits(gxNetworkEventGroup, SNTP_IN_PROGRESS_STATUS);
+            break;
+        }
     }
 }
+
+void SynchronizeSNTP()
+{
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, "a.st1.ntp.br");
+    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
+
+    sntp_set_time_sync_notification_cb(SNTPCallbackFunction);
+    sntp_init();
+}
+
+SNTP_TRIES SNTP_Tries()
+{
+    static uint8_t connection_try = 0;
+    SNTP_TRIES retval = RETRY;
+
+    EventBits_t sntp_status = xEventGroupGetBits(gxNetworkEventGroup);
+
+    if(SNTP_SYNC_STATUS_COMPLETED & sntp_status)
+    {
+        connection_try = 0;
+        retval = CONNECTED;
+        printf("Sucessfully Connected to SNTP server");
+        sntp_stop();
+    }
+    else if(connection_try < MAX_SNTP_CONNECTION_RETRIES)
+    {
+        sntp_restart();
+        connection_try++;
+    }
+    else
+    {
+        retval = CANCELLED;
+        connection_try = 0;
+        printf("Problem during connection to SNTP server");
+        sntp_stop();
+    }
+    xEventGroupClearBits(gxNetworkEventGroup, g_uxSNTPWaitBits);
+
+    return retval;
+}
+/*------------------------------------------------------------------------------------------------------------------*/
+
+/* ---------------------------------------------- Wifi Initialization ------------------------------------------------
+
+    Initialization order defined by the esp idf documentation.
+    Link: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/wifi.html#esp32-wi-fi-station-general-scenario
+    Subtitle: ESP32 Wi-Fi Station General Scenario
+*/
 
 static void WifiEventHandler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void* event_data)
 {
@@ -177,7 +251,7 @@ static void WifiEventHandler(void *event_handler_arg, esp_event_base_t event_bas
 
                 gxSocketConfig.sin_addr.s_addr = event_ip->ip_info.ip.addr;
 
-                snprintf(ip_string, sizeof(ip_string),"state machine step: IP_EVENT_STA_GOT_IP, ip address: %i.%i.%i.%i", IP2STR(event_ip->ip_info.ip));
+                snprintf(ip_string, sizeof(ip_string),"state machine step: IP_EVENT_STA_GOT_IP, ip address: %i.%i.%i.%i", IP2STR(&(event_ip->ip_info.ip)));
                 printString(ip_string);
                 xEventGroupSetBits(gxNetworkEventGroup, WIFI_IP_SET_BIT);
                 break;
@@ -194,164 +268,69 @@ static void WifiEventHandler(void *event_handler_arg, esp_event_base_t event_bas
     }
 }
 
-bool vSocketInitialization()
+bool CheckWifiConnection()
 {
-    bool retVal = false;
-    gxSocketConfig.sin_len = sizeof(struct sockaddr_in);
-    gxSocketConfig.sin_family = AF_INET;
-    gxSocketConfig.sin_port = htons(UDP_PORT);
-
-    if(-1 == (giSocketFd = socket(AF_INET, SOCK_DGRAM, 0)))
+    bool retval = false;
+    xEventGroupWaitBits(gxNetworkEventGroup, g_uxWifiWaitBits, pdTRUE, pdTRUE, 10/portTICK_PERIOD_MS);
+    if(g_uxWifiWaitBits == xEventGroupGetBits(gxNetworkEventGroup))
     {
-        printString("Socket Initialization Error");
+        retval = true;
     }
-    else if(-1 == bind(giSocketFd, (struct sockaddr *) &gxSocketConfig, gxSocketConfig.sin_len))
-    {
-        printString("Socket binding error");
-    }
-    else
-        retVal = true;
-
-    return retVal;
-}
-
-void vSocketHandler(void *pvParameters)
-{
-    UBaseType_t uxHighWaterMark;
-    uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
-    printStringPlusNumber("Remaining stack size (1): ", uxHighWaterMark);
-
-    printString("Initializing socket");
-    xEventGroupWaitBits(gxNetworkEventGroup, g_uxWifiWaitBits, pdTRUE, pdTRUE, portMAX_DELAY);
-
-    uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
-    printStringPlusNumber("Remaining stack size (2): ", uxHighWaterMark);
-    if(vSocketInitialization())
-    {
-        uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
-        printStringPlusNumber("Remaining stack size (3): ", uxHighWaterMark);
-
-        for(;;)
-        {
-            printString("Socket running");
-            uxHighWaterMark = uxTaskGetStackHighWaterMark( NULL );
-            printStringPlusNumber("Remaining stack size (4): ", uxHighWaterMark);
-            vTaskDelay(1000/portTICK_PERIOD_MS);
-        }
-    }
-}
-
-/*-------------------------------------------- SNTP functions ---------------------------------------------*/
-void SNTPCallbackFunction(struct timeval *tv)
-{
-    sntp_sync_status_t sntp_status = sntp_get_sync_status();
-
-    switch(sntp_status)
-    {
-        case SNTP_SYNC_STATUS_RESET:
-        {
-            xEventGroupSetBits(gxNetworkEventGroup, SNTP_RESET_STATUS);
-            break;
-        }
-
-        case SNTP_SYNC_STATUS_COMPLETED:
-        {
-            xEventGroupSetBits(gxNetworkEventGroup, SNTP_CONNECTED_STATUS);
-            break;
-        }
-
-        case SNTP_SYNC_STATUS_IN_PROGRESS:
-        {
-            xEventGroupSetBits(gxNetworkEventGroup, SNTP_IN_PROGRESS_STATUS);
-            break;
-        }
-    }
-}
-
-void SynchronizeSNTP()
-{
-    sntp_setoperatingmode(SNTP_OPMODE_POLL);
-    sntp_setservername(0, "a.st1.ntp.br");
-    sntp_set_sync_mode(SNTP_SYNC_MODE_IMMED);
-
-    sntp_set_time_sync_notification_cb(SNTPCallbackFunction);
-    sntp_init();
-}
-
-SNTP_TRIES SNTP_Tries(EventBits_t sntp_status)
-{
-    static uint8_t connection_try = 0;
-    SNTP_TRIES retval = RETRY;
-
-    if(SNTP_SYNC_STATUS_COMPLETED == sntp_status)
-    {
-        connection_try = 0;
-        SNTP_TRIES = CONNECTED;
-        printf("Sucessfully Connected to SNTP server");
-        sntp_stop();
-    }
-    else if(connection_try < MAX_SNTP_CONNECTION_RETRIES)
-    {
-        sntp_restart();
-        connection_try++;
-    }
-    else
-    {
-        SNTP_TRIES = CANCELLED;
-        connection_try = 0;
-        printf("Problem during connection to SNTP server");
-        sntp_stop();
-    }
-    xEventGroupClearBits(gxNetworkEventGroup, g_uxSNTPWaitBits);
-
     return retval;
 }
-/*------------------------------------------------------------------------------------------------------------------*/
 
-/*
-    Initialization order defined by the esp idf documentation.
-    Link: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-guides/wifi.html#esp32-wi-fi-station-general-scenario
-    Subtitle: ESP32 Wi-Fi Station General Scenario
-*/
-void vWifiInitialization()
+void vInitWiFi(void *pvParameters)
 {
-    xEventGroupClearBits(gxNetworkEventGroup, WIFI_START_BIT| WIFI_CONNECTED_BIT | WIFI_IP_SET_BIT | WIFI_DISCONNECTED | WIFI_IP_LOST_BIT);
+    for(;;)
+    {
+        if(DISCONNECTED == connection_status)
+        {
+            xEventGroupClearBits(gxNetworkEventGroup, WIFI_START_BIT| WIFI_CONNECTED_BIT | WIFI_IP_SET_BIT | WIFI_DISCONNECTED | WIFI_IP_LOST_BIT);
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
+            ESP_ERROR_CHECK(esp_netif_init());
+            ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    gpxNetifWifi = esp_netif_create_default_wifi_sta();
+            gpxNetifWifi = esp_netif_create_default_wifi_sta();
 
-    wifi_init_config_t wifi_init_configuration = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_init_configuration);
+            wifi_init_config_t wifi_init_configuration = WIFI_INIT_CONFIG_DEFAULT();
+            esp_wifi_init(&wifi_init_configuration);
 
-    esp_event_handler_instance_t wifi_event_handler;
-    esp_event_handler_instance_t ip_event_handler;
+            esp_event_handler_instance_t wifi_event_handler;
+            esp_event_handler_instance_t ip_event_handler;
 
 
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, WifiEventHandler, NULL, &wifi_event_handler));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, WifiEventHandler, NULL, &ip_event_handler));
+            ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, WifiEventHandler, NULL, &wifi_event_handler));
+            ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, ESP_EVENT_ANY_ID, WifiEventHandler, NULL, &ip_event_handler));
 
-    wifi_config_t wifi_configuration;
+            wifi_config_t wifi_configuration;
 
-    snprintf((char*) wifi_configuration.sta.ssid, sizeof(wifi_configuration.sta.ssid), NETWORK_SSID);
-    snprintf((char*) wifi_configuration.sta.password, sizeof(wifi_configuration.sta.password), NETWORK_PASSWD);
-    wifi_configuration.sta.scan_method = WIFI_FAST_SCAN;
-    wifi_configuration.sta.bssid_set = false;
-    wifi_configuration.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
-    wifi_configuration.sta.channel = 0;
-    wifi_configuration.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
-    wifi_configuration.sta.pmf_cfg.capable = true;
-    wifi_configuration.sta.pmf_cfg.required = false;
+            snprintf((char*) wifi_configuration.sta.ssid, sizeof(wifi_configuration.sta.ssid), NETWORK_SSID);
+            snprintf((char*) wifi_configuration.sta.password, sizeof(wifi_configuration.sta.password), NETWORK_PASSWD);
+            wifi_configuration.sta.scan_method = WIFI_FAST_SCAN;
+            wifi_configuration.sta.bssid_set = false;
+            wifi_configuration.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+            wifi_configuration.sta.channel = 0;
+            wifi_configuration.sta.sort_method = WIFI_CONNECT_AP_BY_SIGNAL;
+            wifi_configuration.sta.pmf_cfg.capable = true;
+            wifi_configuration.sta.pmf_cfg.required = false;
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_configuration));
+            ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_configuration));
 
-    printString("starting Wifi");
-    ESP_ERROR_CHECK(esp_wifi_start());
+            printString("starting Wifi");
+            ESP_ERROR_CHECK(esp_wifi_start());
+
+            if(CheckWifiConnection())
+            {
+                connection_status = CONNECTED;
+                vTaskDelay(50/portTICK_PERIOD_MS);
+            }
+        }
+    }
 }
+/*-------------------------------------------------------------------------------------------------------------------------------------*/
 
-void InitGPIO()
+void vInitGPIO()
 {
     //GPIO settings
     gpio_config_t gpio_configuration = {GPIO_INPUT_PIN_MASK, GPIO_MODE_INPUT, GPIO_PULLUP_ENABLE, GPIO_PULLDOWN_DISABLE};
@@ -359,6 +338,7 @@ void InitGPIO()
     gxButtonMailbox = xQueueCreate(1, sizeof(uint32_t));
 }
 
+/*--------------------------------------------ULP Functions-----------------------------------------------------------*/
 void CalibrateRTCFastClk(void)
 {
     uint32_t rtc_8md256_period = rtc_clk_cal(RTC_CAL_8MD256, 100);
@@ -368,6 +348,7 @@ void CalibrateRTCFastClk(void)
 
 void DisableConnections()
 {
+    communication_status = DISCONNECTED;
     esp_wifi_stop();
 }
 
@@ -387,11 +368,94 @@ void StartULP()
 
 void InitializeULP(void)
 {
-    ulp_gpio_values = 0;
-    ulp_read_counter = 0;
     CalibrateRTCFastClk();
     StartULP();
     EnterDeepSleep();
+}
+/*------------------------------------------------------------------------------------------------------------------*/
+
+bool HandleHostAnswer()
+{
+    char buffer[50] = {};
+    int correctly_received = 0;
+    recvfrom(giSocketFd, buffer, sizeof(buffer), 0, (struct sockaddr *) &giSocketFd, &size_socket);
+    cJSON *json_answer = cJSON_Parse(buffer);
+    correctly_received = cJSON_GetObjectItem(json_answer,"HostAnswer")->valueint;
+    cJson_Delete(json_answer);
+
+    return correctly_received;
+}
+
+void vReceiveData(COMMUNICATION_STATUS *communication_status)
+{
+    //Waiting for answer from peer
+    for(uint8_t answer_periods = 0; answer_periods < MAX_SOCKET_TRY && communication_status; answer_periods++)
+    {
+        struct pollfd sock_poll_fd;
+        sock_poll_fd.fd = giSocketFd;
+        sock_poll_fd.events = POLLIN;
+
+        if(poll(&sock_poll_fd, 1, 10) > 0)
+        {
+            char buffer[30];
+            uint32_t size_socket = sizeof(struct sockaddr_in);
+
+            if(1 == TreatHostAnswer())
+            {
+                *communication_status = COMMUNICATION_SUCCESSFUL;
+                break;
+            }
+        }
+    }
+}
+
+void vSendData(void *pvParameters)
+{
+    COMMUNICATION_STATUS communication_status = DISCONNECTED;
+    for(;;)
+    {
+        vPrepareData();
+        for(uint8_t socket_try = 0; socket_try < MAX_SOCKET_TRY; socket_try++)
+        {
+            if(CONNECTED == communication_status)
+            {
+                if(vInitSocket())
+                {
+                    communication_status = SOCKET_ESTABLISHED;
+                }
+            }
+            else if(SOCKET_ESTABLISHED == communication_status)
+            {
+                sendto(giSocketFd, (void *)buffer, sizeof(buffer), 0, (struct sockaddr *) &giSocketFd, sizeof(struct sockaddr_in));
+                communication_status = DATA_SENT;
+            }
+            else if(DATA_SENT == communication_status)
+            {
+                vReceiveData(communication_status);
+            }
+            else
+            {
+
+            }
+
+            if(COMMUNICATION_SUCCESSFUL == communication_status)
+            {
+                printf("Data exchange successful");
+                break;
+            }
+
+            //wait 10ms to retry socket establishment
+            vTaskDelay(10/portTICK_PERIOD_MS);
+        }
+
+
+        if(COMMUNICATION_SUCCESSFUL != communication_status)
+        {
+            printf("Error during communication with host");
+        }
+
+        InitializeULP();
+    }
 }
 
 void app_main(void)
@@ -401,9 +465,9 @@ void app_main(void)
 
     gxNetworkEventGroup = xEventGroupCreate();
 
-    InitGPIO();
+    vInitGPIO();
 
-    vWifiInitialization();
+    vInitWiFi();
     xEventGroupWaitBits(gxNetworkEventGroup, g_uxWifiWaitBits, pdTRUE, pdTRUE, portMAX_DELAY);
 
     SynchronizeSNTP();
@@ -412,14 +476,16 @@ void app_main(void)
     {
         xEventGroupWaitBits(gxNetworkEventGroup, g_uxWifiWaitBits, pdFALSE, pdFALSE, 3000/portTICK_PERIOD_MS);
         sntp_stop();
-        sntp_connection_status = SNTP_Tries(gxNetworkEventGroup.uxEventBits);
+        sntp_connection_status = SNTP_Tries();
         /* code */
     } while (CONNECTED != sntp_connection_status || CANCELLED != sntp_connection_status);
 
+    ulp_gpio_values = 0;
+    ulp_read_counter = 0;
     InitializeULP();
 
-    xTaskCreatePinnedToCore(GetData, "GetData", 5*configMINIMAL_STACK_SIZE, NULL, 1, NULL, CORE_1);
-    xTaskCreatePinnedToCore(vSocketHandler, "SocketHandler", 3*configMINIMAL_STACK_SIZE, NULL, 1, NULL, CORE_0);
+    xTaskCreatePinnedToCore(vInitWiFi, "vInitWifi", 3*configMINIMAL_STACK_SIZE, NULL, 2, NULL, CORE0);
+    xTaskCreatePinnedToCore(vSendData, "SendData", 5*configMINIMAL_STACK_SIZE, NULL, 2, NULL, CORE1);
 
     while(1)
     {
